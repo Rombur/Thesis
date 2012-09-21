038f27d464af0a3b9129bbfd38360cafdced2487
Bruno Turcksin <bruno.turcksin@gmail.com>
2012-09-21 16:07:28 -0500

    Before Morel's correction.

--- Janus/convergence.py
+++ Janus/convergence.py
@@ -43,6 +43,35 @@ glc_zone_3 = np.array([np.abs(0.852173299226-glc_ref_3),
 glc_zone_4 = np.array([np.abs(19.1355791976-glc_ref_4),
   np.abs(19.1611742042-glc_ref_4),np.abs(19.1787388738-glc_ref_4),
   np.abs(19.18505985-glc_ref_4),np.abs(19.1880017418-glc_ref_4)])
+  
+bld_dof = np.array([3136.,6400.,14400.,25600.,50176.])
+
+bld_ls_zone_1 = np.array([np.abs(11.9020839282-ls_ref_1),
+  np.abs(11.9295860381-ls_ref_1),np.abs(11.9442764001-ls_ref_1),
+  np.abs(11.9491133437-ls_ref_1),np.abs(11.9518831301-ls_ref_1)])
+bld_ls_zone_2 = np.array([np.abs(0.553699159282-ls_ref_2),
+  np.abs(0.547286164022-ls_ref_2),np.abs(0.543862916701-ls_ref_2),
+  np.abs(0.542728993852-ls_ref_2),np.abs(0.54207476615-ls_ref_2)])
+bld_ls_zone_3 = np.array([np.abs(0.860803663537-ls_ref_3),
+  np.abs(0.847340025272-ls_ref_3),np.abs(0.840187506983-ls_ref_3),
+  np.abs(0.837852346013-ls_ref_3),np.abs(0.836525673494-ls_ref_3)])
+bld_ls_zone_4 = np.array([np.abs(19.0817209606-ls_ref_4),
+  np.abs(19.140982451-ls_ref_4),np.abs(19.171619975-ls_ref_4),
+  np.abs(19.1814641939-ls_ref_4),np.abs(19.1870123946-ls_ref_4)])
+
+bld_glc_zone_1 = np.array([np.abs(11.9023773431-glc_ref_1),
+  np.abs(11.9298080094-glc_ref_1),np.abs(11.9444478841-glc_ref_1),
+  np.abs(11.9492656206-glc_ref_1),np.abs(11.9520251548-glc_ref_1)])
+bld_glc_zone_2 = np.array([np.abs(0.553610986043-glc_ref_2),
+  np.abs(0.547216373242-glc_ref_2),np.abs(0.543806020831-glc_ref_2),
+  np.abs(0.54267690842-glc_ref_2),np.abs(0.54202514266-glc_ref_2)])
+bld_glc_zone_3 = np.array([np.abs(0.860709568176-glc_ref_3),
+  np.abs(0.847274597823-glc_ref_3),np.abs(0.840142347796-glc_ref_3),
+  np.abs(0.837815151926-glc_ref_3),np.abs(0.836493044091-glc_ref_3)])
+bld_glc_zone_4 = np.array([np.abs(19.0821894179-glc_ref_4),
+  np.abs(19.1413269727-glc_ref_4),np.abs(19.1718786758-glc_ref_4),
+  np.abs(19.181690221-glc_ref_4),np.abs(19.1872205875-glc_ref_4)])  
+  
 
 fig = pylab.figure(num=1)
 ax = fig.add_subplot(111)
@@ -59,5 +88,21 @@ ax2.set_xlabel('log(# of unknowns)')
 ax2.set_ylabel('log(error)')
 ax2.legend(('Zone 1','Zone 2','Zone 3','Zone 4','Linear'),loc='upper left')
 ax2.grid(True)
+
+fig3 = pylab.figure(num=3)
+ax3 = fig3.add_subplot(111)
+ax3.loglog(bld_dof,bld_ls_zone_1,'s-',bld_dof,bld_ls_zone_2,'s-',bld_dof,bld_ls_zone_3,'s-',bld_dof,bld_ls_zone_4,'s-',[3000,60000],[0.07,0.0035])
+ax3.set_xlabel('log(# of unknowns)')
+ax3.set_ylabel('log(error)')
+ax3.legend(('Zone 1','Zone 2','Zone 3','Zone 4','Linear'),loc='upper left')
+ax3.grid(True)
+
+fig4 = pylab.figure(num=4)
+ax4 = fig4.add_subplot(111)
+ax4.loglog(dof,glc_zone_1,'s-',dof,glc_zone_2,'s-',dof,glc_zone_3,'s-',dof,glc_zone_4,'s-',[3000,60000],[0.07,0.0035])
+ax4.set_xlabel('log(# of unknowns)')
+ax4.set_ylabel('log(error)')
+ax4.legend(('Zone 1','Zone 2','Zone 3','Zone 4','Linear'),loc='upper left')
+ax4.grid(True)
 
 pylab.show()
